897253922b6bb7e1e611802924f39b5d702e8805
HuuNgo12 <ngovanhuu1602@gmail.com>
2023-11-01 14:58:37 +0700

    SideBar

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -7,7 +7,21 @@ import { useCartContext } from '../context/cart_context'
 import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
-  return <h4>cart buttons </h4>
+  return  <Wrapper className='cart-btn-wrapper'>
+    <Link to='/cart' className='cart-btn'>
+      Cart
+      <span className='cart-container'>
+        <FaShoppingCart/>
+        <span className='cart-value'>
+          12
+        </span>
+      </span>
+    </Link>
+    <button type='button' className='auth-btn'>
+      Login
+      <FaUserPlus/>
+    </button>
+  </Wrapper>
 }
 
 const Wrapper = styled.div`
@@ -22,7 +36,6 @@ const Wrapper = styled.div`
     letter-spacing: var(--spacing);
     color: var(--clr-grey-1);
     display: flex;
-
     align-items: center;
   }
   .cart-container {
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Auth from './AuthWrapper'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Private from './PrivateRoute'
+
+export { Home, About, Cart, Products,  SingleProduct, Auth, Checkout, Error, Private, }
