3d3390bd0c3d81fe50b922020d7ef75873cbb310
HuuNgo12 <ngovanhuu1602@gmail.com>
2024-01-02 15:54:15 +0700

    Completed

--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//
-//
 
 root.render(
     <Auth0Provider
-        domain="dev-r7ug0npb0jfxvxit.us.auth0.com"
-        clientId="W3D6WqAZBtRiMl35oIfXdPN370dvleYg"
+        domain={process.env.REACT_APP_AUTH_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
         authorizationParams={{ redirect_uri: window.location.origin }}
         cacheLocation="localstorage"
     >
